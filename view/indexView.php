efd6f9613a8a27960669c32177f7101389cf4540
Mathieu Deriquehem <mathieuderiquehem@gmail.com>
2020-10-27 14:35:26 +0100

    Update Doc with diagrams

--- view/indexView.php
+++ view/indexView.php
@@ -1,1 +1,14 @@
+<?php
+$site_title = "Au lecteur joyeux";
+include "view/template/header.php";
+?>
+
+
+
 <p>Vos livre au catalogue s'affichent sur cette page</p>
+
+
+
+<?php
+include "view/template/footer.php";
+?>
---
+++ view/template/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <title><?php echo $site_title;?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="La bibliothèque 'Au lecteur joyeux' vous fait vivre des aventures passionnantes au travers des livres">
+    <meta name="keywords" content="Bibliothèque, lecteur, joyeux, aventures, passionnantes, livres">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="apple-touch-icon" href="public/img/logo-bibliothèque.png">
+    <link rel="shortcut icon" href="public/img/logo-bibliothèque.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Grandstander&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/main.css">
+  </head>
+
+  <body>
+    <!-- Header -->
+    <header class="jumbotron jumbotron-fluid bg-info">
+        <section class="container">
+            <div class="container text-center">
+                <h1 class="display-4 font-weight-bold">Au lecteur joyeux</h1>
+            </div>
+        </section>
+    </header>
